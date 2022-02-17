c452f938b13bfa767ac1c0ddcd87fba9f2664368
Viktor Varju <66560709+varjuviktor69@users.noreply.github.com>
2022-02-17 16:55:10 +0100

    PlayerContext class done in Models folder. Nuget packages added. Playercontext dependency added in Startup.cs. Database created.

---
+++ PlayersAPI/PlayersAPI/Migrations/20220217155235_Initial.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlayersAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Clubs",
+                columns: table => new
+                {
+                    ClubID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClubName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ManagerName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    League = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clubs", x => x.ClubID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PlayerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    ClubID = table.Column<int>(type: "int", nullable: true),
+                    FullName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Height = table.Column<decimal>(type: "decimal(3,1)", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<decimal>(type: "decimal(3,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PlayerID);
+                    table.ForeignKey(
+                        name: "FK_Players_Clubs_ClubID",
+                        column: x => x.ClubID,
+                        principalTable: "Clubs",
+                        principalColumn: "ClubID",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_ClubID",
+                table: "Players",
+                column: "ClubID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Clubs");
+        }
+    }
+}
---
+++ PlayersAPI/PlayersAPI/Models/PlayerContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PlayersAPI.Models
+{
+    public class PlayerContext : DbContext
+    {
+        public PlayerContext (DbContextOptions<PlayerContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder
+        optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;userid = root; database = db_elso", ServerVersion.Parse("10.4.21 - mariadb"));
+            }
+        }
+    }
+}
