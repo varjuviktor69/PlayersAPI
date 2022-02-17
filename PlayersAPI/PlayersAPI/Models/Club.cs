c06a3e75a46494986e2eb3cc47081583ecd57e9e
Viktor Varju <66560709+varjuviktor69@users.noreply.github.com>
2022-02-17 16:16:03 +0100

    Player and Club classes done in Models folder

---
+++ PlayersAPI/PlayersAPI/Models/Club.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlayersAPI.Models
+{
+    public class Club
+    {
+        [Key]
+        public int ClubID { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string ClubName { get; set; }
+        [StringLength(100)]
+        public string ManagerName { get; set; }
+        [StringLength(100)]
+        public string League { get; set; }
+    }
+}
---
+++ PlayersAPI/PlayersAPI/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlayersAPI.Models
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerID { get; set; }
+        public int Age { get; set; }
+        [ForeignKey("ClubID")]
+        public Club ClubID { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+        [Column(TypeName ="decimal(3,1)")]
+        public decimal Height { get; set; }
+        public int Salary { get; set; }
+        [Column(TypeName = "decimal(3,2)")]
+        public decimal Weight { get; set; }
+
+    }
+}
