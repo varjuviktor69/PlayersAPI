ee1a00fe5a8870c597f1f7061bcd4ce2e40cc75b
Viktor Varju <66560709+varjuviktor69@users.noreply.github.com>
2022-02-17 18:12:33 +0100

    PlayersController létrejött

--- PlayersAPI/PlayersAPI/Models/Player.cs
+++ PlayersAPI/PlayersAPI/Models/Player.cs
@@ -13,10 +13,10 @@ namespace PlayersAPI.Models
         [Required]
         [StringLength(100)]
         public string FullName { get; set; }
-        [Column(TypeName ="decimal(3,1)")]
+        [Column(TypeName ="decimal(3,2)")]
         public decimal Height { get; set; }
         public int Salary { get; set; }
-        [Column(TypeName = "decimal(3,2)")]
+        [Column(TypeName = "decimal(4,1)")]
         public decimal Weight { get; set; }
 
     }
